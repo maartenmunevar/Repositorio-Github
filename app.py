9ab34b07a75a4c95dacc84e677c7800c715b8613
Jean Munevar Uniandes <j.munevarb@uniandes.edu.co>
2025-08-12 01:49:25 +0300

    Many stuff

--- app.py
+++ app.py
@@ -271,4 +271,5 @@ def update_output_div(date, hour, proy):
 # Run the server
 if __name__ == "__main__":
     #Be aware that it is not app.run_server
+    #Damn so many things
     app.run(debug=True)
